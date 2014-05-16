79b9e447beda98404c0abe98afa4457d4086943d
Christian Muller <christian.mueller@heig-vd.ch>
2014-05-16 10:05:50 +0200

    - Demo script updates

--- demo/demo.sh
+++ demo/demo.sh
@@ -4,13 +4,13 @@ echo "Binding gpsd to GPS device"
 sudo gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock
 
 echo "Starting video visualisation"
-raspivid -p -n -t 0 -w 1280 -h 720 -fps 30 -b 500000 &
+./raspivid -p -n -t 0 -w 1280 -h 720 -fps 30 -b 500000 &
 
 echo "Starting data recorder. Data is saved in /opt/QtOSD/bin"
-sudo python recorder/recorder.py &
+sudo python ../recorder/recorder.py &
 
 echo "Starting data visualisation overlay"
-demo/QtOSD &
+QtOSD/bin/QtOSD &
 
 echo "All services started, press ENTER to end"
 
