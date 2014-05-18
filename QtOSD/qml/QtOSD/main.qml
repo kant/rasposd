c02c3300708b3e8e3435e655286e3316d250096d
Christian Muller <christian.mueller@heig-vd.ch>
2014-05-18 11:25:29 +0200

    Update OS layout and add data fusion code

--- QtOSD/qml/QtOSD/main.qml
+++ QtOSD/qml/QtOSD/main.qml
@@ -2,41 +2,107 @@ import QtQuick 2.2
 import FileIO 1.0
 
 Rectangle {
-    width: 1280
-    height: 720
+    width: 720
+    height: 576
 
-    property int altitude: 0
-    property int yaw: 0
-    property int pitch: 0
-    property int roll: 0
-    property int speed: 0
+    property double altitude: 0
+    property double yaw: 0
+    property double pitch: 0
+    property double roll: 0
+    property double speed: 0
 
-    property int font_size: 30
+    property double roll_max: 2.5883750748
+    property double roll_min: -3.7320618556
+
+    property double pitch_max: 1.4
+    property double pitch_min: -1.65
+
+    property int font_size: 15
+
 
     color: "transparent"
 
+	FontLoader { id: myfont; source: "/home/pi/pilotage-fpv/Arial.ttf" }
+	// arial ne marche pas si je ne la charge pas avant.
 
     Text {
         id: lblTemperature
         font.pointSize: font_size
-        text: "Temperature"
         horizontalAlignment: Text.AlignRight
+	color: "white"
+	style: Text.Outline; styleColor: "black"
+     	font.family: "arial"
+    }
 
+    Text {
+        id: lblLatitude
+        font.pointSize: font_size
+        text: "46.540386 N "
+	color: "white"
+	style: Text.Outline; styleColor: "black"
+     	font.family: "arial"
+        anchors.right: parent.horizontalCenter
+    }
+
+    Text {
+        id: lblLongitude
+        font.pointSize: font_size
+        text: " 6.631568 E"
+	color: "white"
+	style: Text.Outline; styleColor: "black"
+     	font.family: "arial"
+        anchors.left: parent.horizontalCenter
+//        anchors.top: lblLatitude.bottom
     }
 
 
+    Text {
+        id: lblDate
+        font.pointSize: font_size
+        text: "2014.05.13 - "
+        horizontalAlignment: Text.AlignRight
+	color: "white"
+	style: Text.Outline; styleColor: "black"
+     	font.family: "arial"
+	anchors.right: lblHeure.left
+	anchors.bottom: parent.bottom
+    }
 
 
+    Text {
+        id: lblHeure
+        font.pointSize: font_size
+        text: "13:37:23"
+        horizontalAlignment: Text.AlignRight
+	color: "white"
+	style: Text.Outline; styleColor: "black"
+     	font.family: "arial"
+        anchors.right: parent.right
+        anchors.bottom: parent.bottom
+    }
 
+//    Text {
+//        id: lblroll
+//        font.pointSize: font_size
+//        text: roll
+//	color: "white"
+//	style: Text.Outline; styleColor: "black"
+//     	font.family: "arial"
+//        horizontalAlignment: Text.AlignRight
+//        anchors.right: parent.right
+//    }
 
 
     Rectangle {
         id: horizon
         width: 300
-        height: 5
-        color: "red"
+        height:4
+        color: "#80008000"
         anchors.centerIn: parent
         rotation: roll
+	border.color: "#C0FFFFFF"
+	border.width: 1
+	// smooth: true // did not work
     }
 
 
@@ -45,40 +111,56 @@ Rectangle {
 
     Ruler {
         id: altitude_ruler
-        width: 50
-        height: 400
+        width: 30
+        height: 300
 
         anchors.centerIn: parent
-        anchors.horizontalCenterOffset: 400
+        anchors.horizontalCenterOffset: 300
 
-        property int value: 834
+        property double value: 834
         property int nb_big_slots: 5
         property int nb_small_slots: 5
 
         property int step: 50
 
-        x: 330
-        y: 160
     }
 
 
     Ruler {
         id: velocity_ruler
-        width: 50
-        height: 400
+        width: 30
+        height: 300
 
         anchors.centerIn: parent
-        anchors.horizontalCenterOffset: -400
+        anchors.horizontalCenterOffset: -300
 
+        rotation: 180
 
-        property int value: 0
+        property double value: 0
         property int nb_big_slots: 5
         property int nb_small_slots: 1
 
         property int step: 100
 
-        x: 827
-        y: 160
+    }
+
+    Ruler{
+        id: direction_ruler
+        width: 20
+        height: 500
+
+        anchors.centerIn: parent
+        anchors.verticalCenterOffset: -150
+
+        rotation: -90
+
+        property double value: 0
+        property int nb_big_slots: 5
+        property int nb_small_slots: 1
+
+        property int step: 45
+
+        property double cycle: 360
     }
 
 
@@ -88,24 +170,33 @@ Rectangle {
 
         //lblSpeed.text = data_imu.getValue(FileIO.SPEED) + " m/s";
 
-        horizon.rotation = data_imu.getValue(FileIO.ROLL)*180;
+        horizon.rotation = (parseFloat(data_imu.getValue(FileIO.PITCH))/(pitch_max+Math.abs(pitch_min)))*-360;
+        lblTemperature.text = data_imu.getValue(FileIO.TEMPERATURE) + "°C";
+
+        // lblroll.text = data_imu.getValue(FileIO.PITCH);
 
         velocity_ruler.value += 1
-        altitude_ruler.value += 1
+        altitude_ruler.value = parseFloat(data_imu.getValue(FileIO.PRESSURE));
+        direction_ruler.value = data_imu.getValue(FileIO.YAW)/6*360;
+
+        //altitude_ruler.value += 2
+        //direction_ruler.value += 3
+        //velocity_ruler.value = 0
     }
 
     FileIO {
         id: data_imu
-        source: "data_imu.csv"
+        source: "/home/pi/pilotage-fpv/demo/QtOSD/bin/data_imu.csv"
         onError: console.log(msg)
     }
 
     Component.onCompleted: {
+        data_imu.open();
         data_imu.read();
     }
 
     Timer {
-        interval: 40 // 25 Hz
+        interval: 20 // 50 Hz
         onTriggered: refresh()
         repeat: true
         running: true
--- recorder/position/PositionRecorder.py
+++ recorder/position/PositionRecorder.py
@@ -43,23 +43,20 @@ class ImuDataset():
         self.pressure = data[11]
 
     def equals(self, dataset):
-        return self.time == dataset.time and \
-               self.pitch == dataset.pitch and \
-               self.roll == dataset.roll and \
-               self.yaw == dataset.yaw and \
-               self.gyro_scaled_x == dataset.gyro_scaled_x and \
-               self.gyro_scaled_y == dataset.gyro_scaled_y and \
-               self.gyro_scaled_z == dataset.gyro_scaled_z and \
-               self.accel_scaled_x == dataset.accel_scaled_x and \
-               self.accel_scaled_y == dataset.accel_scaled_y and \
-               self.accel_scaled_z == dataset.accel_scaled_z and \
-               self.temperature == dataset.temperature and \
-               self.pressure == dataset.pressure
+        return self.pitch == dataset.pitch and \
+            self.roll == dataset.roll and \
+            self.yaw == dataset.yaw and \
+            self.accel_scaled_x == dataset.accel_scaled_x and \
+            self.accel_scaled_y == dataset.accel_scaled_y and \
+            self.accel_scaled_z == dataset.accel_scaled_z and \
+            self.temperature == dataset.temperature and \
+            self.pressure == dataset.pressure
 
 
 class GpsDataset():
 
     def __init__(self):
+        self.time = 0
         self.ept = 0
         self.latitude = 0
         self.longitude = 0
@@ -75,7 +72,25 @@ class GpsDataset():
         self.epc = 0
         self.mode = 0
 
-    def set(self, data):
+    def set(self, fix):
+        self.time = fix.time
+        self.ept = fix.ept
+        self.latitude = fix.latitude
+        self.longitude = fix.longitude
+        self.altitude = fix.altitude
+        self.epx = fix.epx
+        self.epy = fix.epy
+        self.epv = fix.epv
+        self.track = fix.track
+        self.speed = fix.speed
+        self.climb = fix.climb
+        self.epd = fix.epd
+        self.eps = fix.eps
+        self.epc = fix.epc
+        self.mode = fix.mode
+
+    def equals(self, dataset):
+        return self.time == dataset.time
 
 
 class PositionRecorder(threading.Thread):
@@ -85,9 +100,41 @@ class PositionRecorder(threading.Thread):
         self.bus = smbus.SMBus(i2c_raspberry_pi_bus_number())
         self.imu = GY88(self.bus, gyro_address, compass_address, barometer_address, "GY88")
         self.gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-        self.output = open(directory + 'data_pos.csv', "wb")
+        #self.output = open(directory + 'data_pos.csv', "wb")
+        self.output = open('/dev/stdout', "wb")
         self.writer = csv.writer(self.output, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
         self.running = False
+
+        self.imu_data = NaN
+        self.imu_data_old = NaN
+        self.gps_data = NaN
+        self.gps_data_old = NaN
+
+        self.imu_changed = False
+        self.gps_changed = False
+
+        self.time = 0
+        self.gps_time = 0
+        self.imu_time = 0
+
+        self.speed = 0
+        self.climb = 0
+
+        self.latitude = 0
+        self.longitude = 0
+
+        self.altitude = 0
+        self.pressure_ref = 0
+
+        self.temperature = 0
+
+        self.track = 0
+        self.mode = 0
+        self.sats = NaN
+
+        self.pitch = 0
+        self.roll = 0
+        self.yaw = 0
 
     def run(self):
         self.running = True
@@ -107,18 +154,58 @@ class PositionRecorder(threading.Thread):
         while self.running:
             # Read data from sensors
 
+            # Save old data
             self.imu_data_old = self.imu_data
             self.gps_data_old = self.gps_data
 
+            # Get new data
             self.imu_data.set(self.imu.read_all())
-            self.gps_data.set(self.gpsd.read())
+            self.gpsd.read()
+            self.gps_data.set(self.gpsd.fix)
+
+            self.imu_changed = not self.imu_data.equals(self.imu_data_old)
+            self.gps_changed = not self.gps_data.equals(self.gps_data_old)
 
             # Compute final data
 
+            if self.gps_changed:
+                self.time = self.gps_data.time
+                self.gps_time = self.time
 
+                self.speed = self.gps_data.speed
+                self.climb = self.gps_data.climb
 
-            self.temperature = self.imu.temperature
+                self.latitude = self.gps_data.latitude
+                self.longitude = self.gps_data.longitude
 
+                self.altitude = self.gps_data.altitude
+                self.pressure_ref = self.imu_data.pressure
+
+                self.temperature = self.gps_data.temperature
+
+                self.track = self.gps_data.track
+                self.mode = self.gps_data.mode
+                self.sats = self.gps_data.sats
+            else:
+                if self.imu_changed:
+
+                    self.imu_time.delta = self.imu_data.time - self.imu_time
+                    self.imu_time = self.imu_data.time
+
+                    self.time += self.imu_time.delta
+
+                    self.speed += self.imu_data.accel_scaled_x*self.imu_time.delta
+                    self.climb += self.imu_data.accel_scaled_z*self.imu_time.delta
+
+                    self.altitude = self.gps_data.altitude + (self.imu_data.pressure-self.pressure_ref)*8
+
+            if self.imu_changed:
+
+                self.pitch = self.imu_data.pitch
+                self.roll = self.imu_data.roll
+                self.yaw = self.imu_data.yaw
+
+                self.temperature = self.imu_data.temperature
 
             # Write data line to CSV file
             self.writer.writerow([
