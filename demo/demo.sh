8ee65caa5cd22aa910055773ff71c9378de4e6e7
Christian Muller <christian.mueller@heig-vd.ch>
2014-06-03 14:59:00 +0200

    Fix demo

--- demo/demo.sh
+++ demo/demo.sh
@@ -3,14 +3,16 @@
 echo "Binding gpsd to GPS device"
 sudo gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock
 
+cd ../recorder
+
 echo "Starting video visualisation"
-./raspivid -p -n -t 0 -w 1280 -h 720 -fps 30 -b 500000 -rot 180 &
+../demo/raspivid -p -n -t 0 -w 1280 -h 720 -fps 30 -b 500000 -rot 180 &
 
 echo "Starting data recorder. Data is saved in QtOSD/bin"
-sudo python ../recorder/recorder_demo.py &
+sudo python recorder_fusion.py &
 
 echo "Starting data visualisation overlay"
-QtOSD/bin/QtOSD -platform eglfs &
+../demo/QtOSD/bin/QtOSD -platform eglfs &
 
 echo "All services started, press ENTER to end"
 read val
